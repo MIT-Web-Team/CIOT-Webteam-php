7a564ee3a5135fa848a11c6f83f69ad8c7ad18b3
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-02-13 18:05:49 +0530

    Add homeImage component

---
+++ src/component/homeImage.php
@@ -0,0 +1,9 @@
+<div class="mainContainer">
+    <div class="mask">
+        <div class="content">
+            <span class="small">Centre for</span>
+            <span class="big">Internet of Things</span>
+            <span class="small">Anna University</span>
+        </div>
+    </div>
+</div>
