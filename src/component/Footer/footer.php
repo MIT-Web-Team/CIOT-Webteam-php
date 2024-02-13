9ac586e42a526717c481c7443b3750295c399f41
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-02-13 18:05:25 +0530

    Add footer component

---
+++ src/component/Footer/footer.php
@@ -0,0 +1,76 @@
+ <footer class="page-footer font-small blue pt-4">
+   <div class="container-fluid text-center text-md-left">
+     <div class="row">
+       <div class="footer-list col-md-3 mb-md-0 mb-3">
+         <h5 class="text-uppercase">About Us</h5>
+         <ul class="list-unstyled">
+           <li>
+             <a href="/ciot">Home</a>
+           </li>
+           <li>
+             <a href="/ciot/facilities">Facilities</a>
+           </li>
+           <li>
+             <a href="/ciot/member">Members</a>
+           </li>
+           <li>
+             <a href="/ciot/equipments">Equipments</a>
+           </li>
+           <li>
+             <a href="/ciot/event">Conference</a>
+           </li>
+           <li>
+             <a href="/ciot/event">Workshop</a>
+           </li>
+         </ul>
+       </div>
+       <hr class="clearfix w-100 d-md-none pb-0" />
+       <div class="footer-list col-md-3 mb-md-0 mb-3">
+         <h5 class="text-uppercase">Research</h5>
+         <ul class="list-unstyled">
+           <li>
+             <a href="/ciot/member">List of Supervisors</a>
+           </li>
+           <li>
+             <a href="/ciot/research">Theses & Publications</a>
+           </li>
+           <li>
+             <a href="#!">Projects</a>
+           </li>
+         </ul>
+       </div>
+       <hr class="clearfix w-100 d-md-none pb-0" />
+       <div class="footer-list col-md-3 mb-md-0 mb-3">
+         <h5 class="text-uppercase">Contacts</h5>
+         <div class="footer-contacts-list">
+           <div class="footer-contact-list-elements">
+             <!-- You can replace the icons with images or other HTML elements -->
+             <div style="width: 30;"></div>
+             <div style="line-height: 1.5;">
+               <div>Anna University, MIT Campus,</div>
+               <div>Chromepet, Chennai</div>
+               <div>600044</div>
+             </div>
+           </div>
+           <div class="footer-contact-list-elements">
+             <div style="width: 30;"></div>
+             <div>
+               <div>044 1234 5678</div>
+             </div>
+           </div>
+           <div class="footer-contact-list-elements">
+             <div style="width: 30;"></div>
+             <div>
+               <div>044 1234 5678</div>
+             </div>
+           </div>
+         </div>
+       </div>
+       <hr class="clearfix w-100 d-md-none pb-0" />
+     </div>
+   </div>
+   <div class="footer-copyright text-center py-3">
+     © 2023 Copyright
+     <a href="#"> MIT WebTeam</a>
+   </div>
+ </footer>
