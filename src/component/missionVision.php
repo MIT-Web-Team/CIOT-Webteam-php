770fe9f1d9ef60f6b545a519ce91459299132b9b
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-02-13 22:30:33 +0530

    Add missionVision component

---
+++ src/component/missionVision.php
@@ -0,0 +1,74 @@
+<div class="MissionVision">
+    <div class="vmMask">
+        <div class="vmContent">
+            <div class="boxContainer">
+                <div class="boxTitle">Vision</div>
+                <div class="boxContent" style="text-align: justify;">
+                    <ul class="pointerList">
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">To carry out cutting edge Trans-disciplinary research in IoT and develop indigenous solution to meet out societal requirement.</div>
+                        </li>
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">To impart high quality training to students, researchers, faculties and industrial professionals and offer consultancy to industries and government sectors.</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="boxContainer">
+                <div class="boxTitle">Mission</div>
+                <div class="boxContent" style="text-align: justify;">
+                    <ul class="pointerList">
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">C-IoT shall contribute to the educational, economic and social development by carrying out R &amp; D Projects in the cutting-edge area in IoT involving Trans-disciplinary teams.</div>
+                        </li>
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">Come up with patentable ideas and renowned publications.</div>
+                        </li>
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">Engage in Consultancy work in the area of IoT to cater to the needs of the industries.</div>
+                        </li>
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">Provide a strong foundation in IoT through training programs to the students, researchers, faculties and industrial professionals.</div>
+                        </li>
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">Promote and guide aspiring Entrepreneurs in the area of IoT for their establishment.</div>
+                        </li>
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">Transfer IoT Technology and its application to Government Departments and various users through Pilot Projects/ Operational Projects.</div>
+                        </li>
+                        <li class="points">
+                            <div class="pointerList-pointerLogo" style="padding-right: 2%; padding-top: 0.5%;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="25" width="25" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M172,232a4,4,0,0,1-4,4H88a4,4,0,0,1,0-8h80A4,4,0,0,1,172,232Zm40-128a83.59,83.59,0,0,1-32.11,66.06A20.2,20.2,0,0,0,172,186v6a12,12,0,0,1-12,12H96a12,12,0,0,1-12-12v-6a20,20,0,0,0-7.76-15.81A83.58,83.58,0,0,1,44,104.47C43.75,59,80.52,21.09,126,20a84,84,0,0,1,86,84Zm-8,0a76,76,0,0,0-77.84-76C85,29,51.77,63.27,52,104.43a75.62,75.62,0,0,0,29.17,59.43A28,28,0,0,1,92,186v6a4,4,0,0,0,4,4h28V145.66L93.17,114.83a4,4,0,0,1,5.66-5.66L128,138.34l29.17-29.17a4,4,0,1,1,5.66,5.66L132,145.66V196h28a4,4,0,0,0,4-4v-6a28.14,28.14,0,0,1,10.94-22.2A75.62,75.62,0,0,0,204,104Z"></path>
+                                </svg></div>
+                            <div style="text-align: justify;">Provide academic support to undergraduate / postgraduate / Ph.D / M.S / PDF programs of the University in the area of IoT.</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
