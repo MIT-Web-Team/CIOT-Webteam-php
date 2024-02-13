c4d1c477ce9ee48fe6c7e96c79f17317521179af
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-02-13 18:03:48 +0530

    Add navbar component

---
+++ src/component/Navbar/navbar.php
@@ -0,0 +1,38 @@
+<nav class="bg-body-tertiary navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="/ciot">
+            <div class="header-container">
+                <img src="src/assets/img/logo.png" alt="" class="d-inline-block align-top image-logo">
+                <div class="inner">
+                    <span class="title">CENTRE FOR INTERNET OF THINGS</span>
+                    <span class="sub-title">ANNA UNNIVERSITY</span>
+                </div>
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/ciot">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/ciot/facilities">Facilities</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/ciot/member">Members</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/ciot/equipments">Equipment</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/ciot/research">Research</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/ciot/event">Events & Collaborations</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
