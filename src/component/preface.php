8df965841ebe15e9ed2d02b13098d4ec3c53b60c
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-02-13 18:06:11 +0530

    Add preface component

---
+++ src/component/preface.php
@@ -0,0 +1,5 @@
+<div class="prefaceMainContainer">
+    <div class="prefaceContent">
+        The Centre for Internet of Things (C-IoT) intends to promote the Internet of Things (IoT) based Ecosystem that will open numerous avenues in terms of Knowledge Enhancement, Employability, Entrepreneurship, Institute-Industry Collaboration, Indigenous Product Development and Global Recognition. Functioning of the full-fledged autonomous Centre is approved by syndicate of Anna University since 27th of April 2022.
+    </div>
+</div>
