d0f0605687f6692609cb22807da45ec7a3b5388e
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-02-17 20:41:39 +0530

    Update file paths in events

--- src/pages/Events/events.php
+++ src/pages/Events/events.php
@@ -3,16 +3,10 @@
     <link rel="icon" type="image/svg+xml" href="../../assets/annauniversity.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Anna University - CIOT-PHP</title>
-    <link rel="stylesheet" href="../../../index.css">
-    <link rel="stylesheet" href="../../component/Navbar/navbar.css">
-    <link rel="stylesheet" href="../../component/homeImage.css">
-    <link rel="stylesheet" href="../../component/preface.css">
-    <link rel="stylesheet" href="../../component/director.css">
-    <link rel="stylesheet" href="../../component/missionVision.css">
-    <link rel="stylesheet" href="../../component/Footer/footer.css">
-    <link rel="stylesheet" href="../Facilities/facilities.css">
-    <link rel="stylesheet" href="../Members/members.css">
-    <link rel="stylesheet" href="../Members/memberCard.css">
+    <link rel="stylesheet" href="/index.css">
+    <link rel="stylesheet" href="/src/component/Navbar/navbar.css">
+    <link rel="stylesheet" href="/src/component/homeImage.css">
+    <link rel="stylesheet" href="/src/component/Footer/footer.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -22,24 +16,24 @@
 <?php
 $Events = [
     [
-        "image" => "../../assets/img/events/automeet.jpg",
+        "image" => "/src/assets/img/events/automeet.jpg",
         "description" => "One day National level workshop on “IoT in Automobiles” in AUTOMEET 2022, MIT Campus, Anna University",
         "date" => " 21.05.2022",
     ],
     [
-        "image" => "../../assets/img/events/iotfundamentals.jpg",
+        "image" => "/src/assets/img/events/iotfundamentals.jpg",
         "name" => "",
         "description" => "Two days workshop on “Fundamentals of IoT” in collaboration with Department of Electronics Engineering MIT Campus, Anna University.",
         "date" => "17.11.2022 to 18.11.2022",
     ],
     [
-        "image" => "../../assets/img/events/iotconcept.png",
+        "image" => "/src/assets/img/events/iotconcept.png",
         "name" => "",
         "description" => "Six days Faculty Empowerment Programme on “IoT Concepts and Applications” sponsored by Centre for Facultyand Professional Development, Anna University, Chennai.",
         "date" => "30.01.2023 to 04.02.2023",
     ],
     [
-        "image" => "../../assets/img/events/iot5g.png",
+        "image" => "/src/assets/img/events/iot5g.png",
         "name" => "",
         "description" => "Two or Four weeks SummerInternship or Project work 2023 on “IoT and 5G” for the University Department and affiliated college students of Anna University.",
         "date" => "03.07.2023 to 28-07-2023 ",
@@ -48,19 +42,19 @@ $Events = [
 
 $Collaborations = [
     [
-        "image" => "../../assets/img/events/coli1.jpg",
+        "image" => "/src/assets/img/events/coli1.jpg",
         "description" => "Signed MoA with M/s. Enthu Technology Solutions India Pvt. Ltd, Coimbatore, Tamil Nadu, on 16.02.2023",
     ],
     [
-        "image" => "../../assets/img/events/coli2.jpg",
+        "image" => "/src/assets/img/events/coli2.jpg",
         "description" => "Initiated MoU with Centre for Development of Advance Computing (CDAC), Bangalore.",
     ],
     [
-        "image" => "../../assets/img/events/coli3.jpg",
+        "image" => "/src/assets/img/events/coli3.jpg",
         "description" => "Initiated Industrial Collaboration with M/s Data Patterns (India) Pvt. Ltd on 10.03.2023 in the Presence of Mr. Stanley Moses, Managing Director, Data Patterns, San Francisco, USA.",
     ],
     [
-        "image" => "../../assets/img/events/pic5.jpeg",
+        "image" => "/src/assets/img/events/pic5.jpeg",
         "description" => "Initiated Collaboration with Chennai Metropolitan Water Supply and Sewerage Board",
     ],
     [
@@ -72,7 +66,7 @@ $Collaborations = [
 function customCard($data)
 {
     foreach ($data as $index => $val) {
-        $image = !empty($val['image']) ? $val['image'] : '../../assets/img/Noimage.png';
+        $image = !empty($val['image']) ? $val['image'] : '/src/assets/img/Noimage.png';
         $date = isset($val['date']) ? $val['date'] : '';
         $description = isset($val['description']) ? $val['description'] : '';
         echo '<div class="card-1">';
@@ -113,7 +107,3 @@ function customCard($data)
     </div>
 </div>
 <?php include '../../component/Footer/footer.php'; ?>
-<script>
-    var imgElement = document.querySelector('.header-container img');
-    imgElement.setAttribute('src', '../../assets/img/logo.png');
-</script>
