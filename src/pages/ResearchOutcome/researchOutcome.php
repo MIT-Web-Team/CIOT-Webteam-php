c6d957d2c170ca889b4c3d4e52cc1cc2dad8208a
Praveen Prabhu <praveenzues@gmail.com>
2024-02-17 20:43:29 +0530

    Refactor code

--- src/pages/ResearchOutcome/researchOutcome.php
+++ src/pages/ResearchOutcome/researchOutcome.php
@@ -1,19 +1,13 @@
 <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="../../assets/annauniversity.svg" />
+    <link rel="icon" type="image/svg+xml" href="/src/assets/annauniversity.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Anna University - CIOT-PHP</title>
-    <link rel="stylesheet" href="../../../index.css">
-    <link rel="stylesheet" href="../../component/Navbar/navbar.css">
-    <link rel="stylesheet" href="../../component/homeImage.css">
-    <link rel="stylesheet" href="../../component/preface.css">
-    <link rel="stylesheet" href="../../component/director.css">
-    <link rel="stylesheet" href="../../component/missionVision.css">
-    <link rel="stylesheet" href="../../component/Footer/footer.css">
-    <link rel="stylesheet" href="../Facilities/facilities.css">
-    <link rel="stylesheet" href="../Members/members.css">
-    <link rel="stylesheet" href="../Members/memberCard.css">
-    <link rel="stylesheet" href="./researchOutcome.css">
+    <title>Anna University - CIOT</title>
+    <link rel="stylesheet" href="/index.css">
+    <link rel="stylesheet" href="/src/component/Navbar/navbar.css">
+    <link rel="stylesheet" href="/src/component/homeImage.css">
+    <link rel="stylesheet" href="/src/component/Footer/footer.css">
+    <link rel="stylesheet" href="/src/pages/ResearchOutcome/researchOutcome.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -74,7 +68,3 @@ $Publication = [
     ?>
 </div>
 <?php include '../../component/Footer/footer.php'; ?>
-<script>
-    var imgElement = document.querySelector('.header-container img');
-    imgElement.setAttribute('src', '../../assets/img/logo.png');
-</script>
